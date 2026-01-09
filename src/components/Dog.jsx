8704b6cb1ed61d5689e3d3c489708c22972ac49b
ritam-g <ritammaty2006@gmail.com>
2026-01-09 16:09:38 +0530

    Add texture loading with useTexture and apply matcap/normal maps

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,33 +1,67 @@
-import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture,useAnimations } from '@react-three/drei'
 import { useThree } from '@react-three/fiber'
+import { useEffect } from 'react'
+import * as THREE from 'three'
 
 function Dog() {
   // Load the 3D model (this already contains geometry + materials)
   const model = useGLTF('/models/dog.drc.glb')
+  const{actions}=useAnimations(model.animations,model.scene)
+  useEffect(()=>{
+    actions["Take 001"].play()
+  },[actions])
 
-  // Load the normal map image (adds surface detail, not color)
-  const normalMap = useTexture('/dog_normals.jpg')
+
+  const [normalMap,
+    sampleMatCap,
+    
+  ]=(useTexture(["/dog_normals.jpg","/matcap/mat-2.png","/branches_diffuse.jpeg","/branches_normals.jpeg"])).map((texture=>{
+    texture.flipY=false
+    texture.colorSpace=THREE.SRGBColorSpace
+    return texture
+  }))
+
+  const [branchMap,
+    branchNormalMap]=(useTexture(["/branches_diffuse.jpeg","/branches_normals.jpeg"])).map(texture=>{
+    texture.colorSpace=THREE.SRGBColorSpace
+    return texture
+    })
 
   // Access the camera from the Three.js scene
-  const { camera } = useThree()
+  useThree(({camera,scene,gl})=>{
+    camera.position.z = 1
+    gl.toneMapping = THREE.ReinhardToneMapping
+    gl.outputColorSpace=THREE.SRGBColorSpace
+
+  })
 
   // Move the camera slightly forward so the model is visible
-  camera.position.z = 1.5
+  
+  const dogMetarial=new THREE.MeshMatcapMaterial({
+        normalMap:normalMap,
+        
+        matcap:sampleMatCap
+      })
 
+  const branchMatarial=new THREE.MeshMatcapMaterial({
+    normalMap:branchNormalMap,
+        
+        map:branchMap
+  })
   // Go through every object inside the 3D model
   model.scene.traverse((child) => {
     // Check:
-    // 1. Object must be a mesh (geometry + material)
-    // 2. Object name must start with "dog"
+    //! 1. Object must be a mesh (geometry + material)
+    //! 2. Object name must start with "dog"
     if (
       child.isMesh &&
       child.name.toLowerCase().startsWith('dog')
     ) {
-      // Set a base color so the model is visible
-      child.material.color.set('#aaaaaa')
 
-      // Add normal map to create surface details (bumps, depth)
-      child.material.normalMap = normalMap
+      child.material = dogMetarial
+    }
+    else{
+      child.material=branchMatarial
     }
   })
 
@@ -37,12 +71,12 @@ function Dog() {
       <primitive
         object={model.scene}
         scale={2}                      // Resize the model
-        position={[0.5, -1.1, 0]}      // Move the model in the scene
-        rotation={[0, Math.PI / 2.9, 0]} // Rotate the model
+        position={[0.5, -1.2, 0]}      // Move the model in the scene
+        rotation={[0, Math.PI / 4, 0]} // Rotate the model
       />
 
       {/* Mouse controls: rotate / zoom the model */}
-      <OrbitControls />
+      {/* <OrbitControls /> */}
 
       {/* Light that softly lights everything */}
       <ambientLight intensity={0.6} />
